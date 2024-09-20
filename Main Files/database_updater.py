604ece8e9123bf50a41216e38c428b8e3b8c2777
SecOpsWizard <42916447+phantom0004@users.noreply.github.com>
2024-09-20 11:29:57 +0200

    Added more updates, slowly building to the program

--- Main Files/database_updater.py
+++ Main Files/database_updater.py
@@ -1,4 +1,4 @@
-# STILL IN PROGRESS, NOT TO BE USED NOW
+# PROGRAM STLL IN DEVELOPMENT
 
 import setup
 import os
@@ -7,18 +7,26 @@ import datetime
 
 print(f"Database Update Initiated On : {datetime.datetime.now()}")
 
-if not os.path.exists("version_tracking") or not os.path.exists("repo_versions.txt"):
-    sys.exit("[-] Critical File 'version_tracking' and its contents was not found! Please use 'setup.py' to create this file.")
+if not os.path.exists("version_tracking"):
+    sys.exit("[-] Critical Folder 'version_tracking' was not found! Please use 'setup.py' to create this file.")
+else:
+    os.chdir("version_tracking")
 
+if not os.path.exists("repo_versions.txt"):
+    sys.exit("[-] Critical File 'repo_versions.txt' was not found! Please use 'setup.py' to create this file.")
+    
 # Extract both extensive and shortened github links
-github_links = setup.github_links_yara_rules()
-
+github_links = setup.github_links_yara_rules()    
+    
 file_contents = None
 with open("repo_versions.txt" ,"r") as file:
     file_contents = file.read()
     
 if not file_contents:
-    # Logic to come later
-    ...
-    
-print("File is currently in progress . . .")
+    sys.exit("[-] File 'repo_versions.txt' is empty! Please use 'setup.py' to populate this file.")
+
+# Extract GitHub links based on installation type
+installation_type = file_contents.split(":")[1].splitlines()[0].strip()
+github_links = github_links[1] if installation_type == "NanoShield Edition" else github_links[0]
+
+print("Database Updater In progress . . .")
