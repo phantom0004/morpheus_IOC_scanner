4bd4f5469bb85b545079b92a7ff559c3a8f52413
SecOpsWizard <42916447+phantom0004@users.noreply.github.com>
2024-09-25 16:42:22 +0200

    Heavily Revamped Module - IN PROGRESS STILL

--- Main Files/modules/pe_analysis.py
+++ Main Files/modules/pe_analysis.py
@@ -1,6 +1,26 @@
-# PE analysis logic
+"""
+PE Analysis Logic
+Author: Phantom0004 (Daryl Gatt)
+
+Description:
+This section handles the analysis of Portable Executable (PE) files using a dedicated PE analysis library. 
+It provides more precise and detailed inspection of PE structures, making it more suitable than YARA for in-depth PE analysis.
+
+Usage:
+This logic focuses on extracting detailed information from PE files, including headers, sections, imports, and other attributes, 
+to complement or go beyond what YARA pattern matching can achieve.
+
+"""
+
 import pefile
 
-class ExecutableAnalysis():
-    def __init__(self):
+class ExecutableAnalysis:
+    def __init__(self, file_to_scan:str) -> None:
+        self.file_to_scan = file_to_scan
+    
+    def detect_nopslides(self) -> None:
+        pass
+    
+    @staticmethod
+    def extract_sections(section_name:str) -> list:
         pass
