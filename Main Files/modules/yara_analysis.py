97ea0d79051f3d8afccc3aa22000bb158130b1bb
SecOpsWizard <42916447+phantom0004@users.noreply.github.com>
2024-10-04 10:53:59 +0200

    Revamped entire code to fix several issue - NOT FULLY DONE

--- Main Files/modules/yara_analysis.py
+++ Main Files/modules/yara_analysis.py
@@ -28,88 +28,97 @@ except:
 
 # Parent class which handles the core of this file
 class BaseDetection:
-    def __init__(self, file_to_scan:str, scan_type:str="file_analysis") -> None:
-        # Pre-Defined Values - Yara Paths
-        self.yara_database_path = os.path.abspath(os.path.join("yara_rules", "external_yara_rules"))
-        self.file_analysis_path = os.path.abspath(os.path.join("yara_rules", "file_analysis"))
-        
+    def __init__(self, file_to_scan:str, scan_type:str="file_analysis") -> None:        
         # User Defined Value - Program Setup
-        self.scan_type = scan_type
         self.file_to_scan = file_to_scan
+        self.scan_type = scan_type
     
-    # List all files present inside a set for further usage
-    def list_yara_files(self) -> Union[set, str]:
+    # Scan for Yara rules in the Morpheus database    
+    def scan_for_yara_files(self) -> Union[set, str]:
         files = set()
-        folder_path = self.yara_database_path if self.scan_type == "malware_scan" else self.file_analysis_path
+        
+        # Define path for scanning
+        scan_folder = "file_analysis" if self.scan_type == "file_analysis" else "external_yara_rules"
+        folder_path = os.path.abspath(os.path.join("yara_rules", scan_folder))
         
         try:
-            for yara_file in os.listdir(folder_path):
-                files.add(os.path.join(folder_path, yara_file))
+            for (root, _, folder_files) in os.walk(folder_path):
+                for yar_file in folder_files:
+                    files.add(os.path.join(root, yar_file).strip())
         except (PermissionError, OSError):
             pass # Skip as these may be minor I/O errors
         except Exception as err:
             return f"Directory Error : {err}"
-            
-        return files
-    
-    # The first step in scanning - Will compile all the Yara rules
-    @staticmethod            
-    def compile_yara_rules(instance: 'BaseDetection') -> Union[yara.Rules, str]:
-        file_paths = instance.list_yara_files() # Get all current yara rules of scan type
         
-        # File analysis simply does a general scan to the file, the malware scan uses the external yara database
-        try:
-            # Compile all paths in a dictionary to compile simultaneously
-            rules = yara.compile(filepaths={str(index): path for index, path in enumerate(file_paths)})
-            return rules
-        except Exception as err:
-            return f"Compile Error : {err}"
-    
-    # The second step in scanning - Will assess the Yara rules for that file
-    def load_rules(self, yara_object:yara.Rules) -> Union[List[yara.Match], str]:        
+        return files
+        
+    # Will load the Yara rule, find any matches and send the output to a function
+    @staticmethod            
+    def parse_yara_rules(instance: 'BaseDetection') -> List[yara.Match]:
+        file_paths = instance.scan_for_yara_files()  # Get all current YARA rule files for the scan type
+        all_matches = []  # Collect all matches from all YARA rule files
+        
+        if not file_paths:
+            exit(colored("[-] Unable to load file! No YARA files were found. Please ensure you have a folder called 'yara_rules'. Aborting.", "red"))
+        elif "Error" in file_paths:
+            exit(colored(file_paths, "red"))  # This is an error message
+        
+        # Compile rules individually to check for errors; if there's an error, skip the file
+        for path in file_paths:
+            try:
+                # Find matches and add them to the collected list
+                rules = yara.compile(filepath=path)
+                matches = instance.identify_matches(rules)
+            except yara.SyntaxError as err:
+                print(colored(f"[-] File Skipped -> Error when compiling '{os.path.basename(path)}'", "red"))
+                continue  # Error in rule file, skip to avoid crashes
+            
+            if matches and "Error" not in str(matches):
+                all_matches.extend(matches)  # Append matches to the all_matches list
+        
+        return all_matches  # Return all matches from all YARA rules
+                
+    # Will handle returning matches found with the file and ruleset
+    def identify_matches(self, yara_object:yara.Rules) -> Union[List[yara.Match], str]:   
         try:
             matches = yara_object.match(self.file_to_scan, timeout=60)
             return matches  
         except TimeoutError:
             # Scanning took longer then 60 seconds
             print("[!] Timeout Encountered, Skipping . . .")
-            return
+            return None
         except Exception as err:
             return f"Loading Error : {err}"
 
-# Most extensive scan, scans for malware and IOC's based on the Yara database
-class MalwareScan():
-    @staticmethod
-    def generate_terminal_output(yara_matches:list) -> None:
-        print(colored("========= MALWARE ANALYSIS OUTPUT =========", attrs=["bold"]))
+    # Will parse the output and display contents on screen
+    @staticmethod 
+    def evaluate_match(yara_matches:yara.Match) -> Union[None, List[yara.Match]]:
         if not yara_matches or not isinstance(yara_matches, list):
             print(colored("[-] No rules matched this file", "yellow"))
+            return None
         else:
-            for match in yara_matches:
-                if match and match.strings:
-                    print(colored(f"[+] FOUND MATCH FROM: '{match}'", "green"))
-                    
-                    print("Found Strings :")
-                    for yara_string in match.strings:
-                        print(f"\t> {yara_string}")
+            return yara_matches
+
+# Most extensive scan, scans for malware and IOC's based on the Yara database
+class MalwareScan():
+    def __init__(self, rule_matches:List) -> None:
+        self.rule_matches = rule_matches
     
-    # Output will be processed in a document such as a PDF
+    def generate_terminal_output(self) -> None:
+        for match in self.rule_matches:
+            print(match)
+        
+    # Output will be processed in a document
     @staticmethod
-    def generate_document_report(scan_output:yara.Match) -> None:
+    def generate_document_report(scan_output:List) -> None:
+        # Will use either FPDF or reportlab
         pass
 
 # Least intensive scan, the first scan of Morpheus which scans for file information
 class GeneralFileScan():
-    @staticmethod
-    def generate_terminal_output(yara_matches:list) -> None:
-        print(colored("========= FILE ANALYSIS OUTPUT =========", attrs=["bold"]))
-        if not yara_matches or not isinstance(yara_matches, list):
-            print(colored("[-] No rules matched this file", "yellow"))
-        else:
-            for match in yara_matches:
-                if match and match.strings:
-                    print(colored(f"[+] FOUND MATCH FROM: '{match}'", "green"))
-                    
-                    print("Found Strings :")
-                    for yara_string in match.strings:
-                        print(f"\t> {yara_string}")
+    def __init__(self, rule_matches:List) -> None:
+        self.rule_matches = rule_matches
+        
+    def generate_terminal_output(self) -> None:
+        for match in self.rule_matches:
+            print(match)
