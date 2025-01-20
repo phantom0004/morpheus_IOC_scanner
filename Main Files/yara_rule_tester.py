5879041bb0d3d4e649e239bcf12141b29a9c8328
phantom0004 <darylgatt40@gmail.com>
2025-01-20 19:13:04 +0100

    feat: Included PID analysis (in progress)

--- Main Files/yara_rule_tester.py
+++ Main Files/yara_rule_tester.py
@@ -3,7 +3,42 @@
 
 import yara
 import os
+import psutil
+import re
 from termcolor import colored
+
+IS_PID = False
+
+# Identifies any matches based on compiled rules
+def identify_matches(rule_object, data):
+    if IS_PID:
+        return None
+    else:
+        return rule_object.match(data)
+
+# Checks if value is in PID format
+def is_pid(value):
+    """
+        Checks if a given value is a valid process ID (PID) and updates the global status.
+
+        Args:
+            value (str): The value to check, expected to be a string representation of a PID.
+
+        Returns:
+            bool | None:
+                - True: If the value consists only of digits, indicating it is a valid PID format, and updates the global `IS_PID` to True.
+                - None: If the value is not a valid PID format.
+    """
+
+    if bool(re.match(r'^\d+$', value)):
+        global IS_PID
+        IS_PID = True
+        
+        log_message("info", f"PID '{value}' detected. Analyzing process...", "yellow")
+        
+        return True
+    
+    return None
 
 # Return YARA rule or all YARA rules in folder to be compiled
 def get_yara_files(folder_path):
@@ -43,7 +78,7 @@ def get_yara_files(folder_path):
         return None
 
 # Compile YARA rules and return YARA object    
-def compile_yara_rules(path):
+def compile_yara_rules(file_path):
     """
     Compiles one or multiple YARA rules into a YARA rules object.
 
@@ -57,13 +92,12 @@ def compile_yara_rules(path):
     """
     
     compiled_rule = None
-    
-    if type(path) is list:
+    if type(file_path) is list:
         # Compile multiple rules
-        compiled_rule = yara.compile(filepaths={f"rule_{index}" : rules for index, rules in enumerate(path)})
+        compiled_rule = yara.compile(filepaths={f"rule_{index}" : rules for index, rules in enumerate(file_path)})
     else:
         # Compile one rule
-        compiled_rule = yara.compile(path)
+        compiled_rule = yara.compile(file_path)
     
     return compiled_rule
 
@@ -110,40 +144,48 @@ def log_message(level, message, color):
                   attrs=["bold"] if level == "error" else []
     ))
 
-# Validate existance of file/folder path
-def validate_files(paths):
+# Validate existance of file/folder path or PID
+def validate_data(data):
     """
-    Validates the existence of file or folder paths.
+        Validates whether the input is a valid file, folder, or process ID (PID).
 
-    Args:
-        paths (list): A list of file or folder paths to validate.
+        Args:
+            data (str | list): 
+                - A string representing a single PID or a list of file/folder paths to validate.
 
-    Returns:
-        list | None: 
-            - The invalid paths if any are found.
-            - None if all paths are valid.
+        Returns:
+            list | None: 
+                - A list of invalid file or folder paths if any are found in the input.
+                - A list containing the PID if it does not exist.
+                - None if all paths are valid or the PID exists.
     """
     
-    invalid_paths = [path for path in paths if not os.path.exists(path)]
-    return invalid_paths if invalid_paths else None
+    if not IS_PID:
+        invalid_paths = [path for path in data if not os.path.exists(path)]
+        return invalid_paths if invalid_paths else None
+    else:        
+        return None if psutil.pid_exists(data[1]) else [data[1]]
 
 try:
     yara_rule_path = input("Enter the folder path where you have your yara rules > ").strip() or ""
-    test_file_path = input("Enter your test file path > ").strip() or os.path.join("Main Files","test_data","client_summary.html")
+    test_data = input("Enter a file path to test or a program PID to analyze > ").strip() or os.path.join("Main Files","test_data","client_summary.html")
 except Exception as error:
     exit(log_message("error", f"Exception Raised on Input : {error}.", "red"))
 
-validation_output = validate_files([yara_rule_path, test_file_path])
+is_pid(test_data) # Check if a process is being analyzed
+validation_output = validate_data([yara_rule_path, test_data] if not IS_PID else [yara_rule_path, int(test_data)])
     
 if validation_output:
-    exit(log_message("error", f"File Path/s '{validation_output}' could not be found. Please enter the correct path.", "red"))
+    if not IS_PID:
+        exit(log_message("error", f"Path '{validation_output}' value could not be found. Please enter the correct file/folder path.", "red"))
+    else:
+        exit(log_message("error", f"PID '{validation_output}' could not be found. Please enter a valid PID.", "red"))
 
 # Extract all YARA files if path is a directory, else use inputted path
 rules_path = get_yara_files(yara_rule_path)
 
 if rules_path:  
     # Compile and validate all YARA rules  
-    
     compiled_yara_rules = handle_yara_exceptions(compile_yara_rules, rules_path)
     if compiled_yara_rules:
         log_message("success", "YARA rule/s loaded and compiled successfully - No errors", "green")
@@ -153,4 +195,4 @@ else:
     # YARA rules path is invalid
     exit(log_message("error", f"Invalid YARA rule path: '{yara_rule_path}'. Please provide a valid file or directory.", "red"))
     
-print("Script is currently under development ... Come back later!")
+# matches = identify_matches(compiled_yara_rules, test_data)
